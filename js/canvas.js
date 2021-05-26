37a6d63223c4861c2da9af7d098f8512540499d0
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2021-05-26 12:18:13 +0200

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -12,24 +12,13 @@ class Canvas {
 		this.ctx = this.canvas.getContext("2d");
 		this.callback = callback;
 
-		//this.frameChanges = false;
-		//this.frameCounter = 0;
-
 		this.dimensions = new Vector2(width, height);
-
-		//this.clear();
-		//this.saveImage();
 
 		this.drawLoop();
 
 	}
 	draw() {
-		//const t = this;
 		this.callback(this);
-		//if (t.frameChanges) {
-		//	t.ctx.putImageData(t.image, 0, 0);
-		//	t.frameChanges = false;
-		//}
 	}
 	clear() {
 		this.ctx.fillStyle = "black";
@@ -45,42 +34,16 @@ class Canvas {
 	}
 	drawLoop() {
 		this.draw();
-		//this.frameCounter++;
-		// every 100 ms
-		//if (this.frameCounter % 6 == 0) {
-		//	this.frameCounter = 0;
-		//	this.saveImage();
-		//}
 		requestAnimationFrame(this.drawLoop.bind(this));
 	}
-	//getPixel(pixel) {
-	//	const index = pixel * 4;
-	//	return this.image.data.slice(index, index + 4);
-	//}
-	//setPixel(pixel, rgba) {
-	//	const index = pixel * 4;
-	//	this.image.data[index] = rgba[0]
-	//	this.image.data[index + 1] = rgba[1];
-	//	this.image.data[index + 2] = rgba[2];
-	//	this.image.data[index + 3] = rgba[3];
-	//	this.frameChanges = true;
-	//}
-	//saveImage() {
-	//	this.image = this.ctx.getImageData(0, 0, this.width, this.height);
-	//}
-	//get width() {
-	//	return this.dimensions.x;
-	//}
-	//get height() {
-	//	return this.dimensions.y;
-	//}
+
 }
 
 class Agent {
 	constructor(limits) {
 		this.limits = limits;
-		this.xy = Vector2.range(Vector2.zero, limits);// new Vector2(CANVAS.dimensions.x / 2, CANVAS.dimensions.y / 2);
-		this.angle = Vector2.random(1, -1);
+		this.xy = /*Vector2.range(Vector2.zero, limits);*/ new Vector2(CANVAS.dimensions.x / 2, CANVAS.dimensions.y / 2);
+		this.angle = new Vector2(5, 5);//Vector2.random(1, -1);
 	}
 	move() {
 		const t = this;
@@ -97,16 +60,19 @@ class Agent {
 
 let agents = [];
 let agentColor = "white";
-let agentSpeed = 30;
-let trailDim = 0.05;
-let agentSteerStrength = 16;
+let agentSpeed = 40;
+let trailDim = 0.04;
+let agentSteerStrength = 7;
 
-let quadrantsX = 30 * 10;
-let quadrantsY = 27 * 10;
+// in pixels
+let quadrantSize = 20;
 let quadrants = [];
 let agentQuadrants = [];
 
-const CANVAS = new Canvas("#canvas-output", 900, 900, function(t){
+const CANVAS = new Canvas("#canvas-output", 600, 600, function(t){
+
+	let quadrantsX = t.dimensions.x / quadrantSize;
+	let quadrantsY = t.dimensions.y / quadrantSize;
 
 	// trail effect
 	t.ctx.fillStyle = "rgba(0, 0, 0, " + trailDim + ")";
@@ -115,8 +81,8 @@ const CANVAS = new Canvas("#canvas-output", 900, 900, function(t){
 	quadrants = [];
 	agentQuadrants = [];
 
-	const _optimizedX = 1 / (t.dimensions.x / quadrantsX);
-	const _optimizedY = 1 / (t.dimensions.y / quadrantsY);
+	const _optimizedX = 1 / (t.dimensions.x / quadrantsX),
+		  _optimizedY = 1 / (t.dimensions.y / quadrantsY);
 
 	for (let i = 0; i < agents.length; i++) {
 
@@ -151,56 +117,56 @@ const CANVAS = new Canvas("#canvas-output", 900, 900, function(t){
 			  	quadrant - quadrantsX, 
 			  	quadrant - quadrantsX + 1, 
 			  	quadrant + 1
-			  ]
+		]
 
 		let direction,
 			adjacent;
 
+		// H A
+		if (degrees > 292.5 && degrees <= 337.5) {
+			direction = 7;
+			adjacent = [_all[7], ..._all.slice(0,2)];
+		}
 		// A B
-		if (degrees >= 337.5 || degrees <= 22.5) {
+		else if (degrees > 337.5 || degrees <= 22.5) {
 			direction = 0;
 			adjacent = _all.slice(0, 3);
 		}
 		// B C
-		else if (degrees >= 22.5 && degrees <= 67.5) {
+		else if (degrees > 22.5 && degrees <= 67.5) {
 			direction = 1;
 			adjacent = _all.slice(1, 4);
 		}
 		// C D
-		else if (degrees >= 67.5 && degrees <= 112.5) {
+		else if (degrees > 67.5 && degrees <= 112.5) {
 			direction = 2;
 			adjacent = _all.slice(2, 5);
 		}
 		// D E
-		else if (degrees >= 112.5 && degrees <= 157.5) {
+		else if (degrees > 112.5 && degrees <= 157.5) {
 			direction = 3;
 			adjacent = _all.slice(3, 6);
 		}
 		// E F
-		else if (degrees >= 157.5 && degrees <= 202.5) {
+		else if (degrees > 157.5 && degrees <= 202.5) {
 			direction = 4;
 			adjacent = _all.slice(4, 7);
 		}
 		// F G
-		else if (degrees >= 202.5 && degrees <= 247.5) {
+		else if (degrees > 202.5 && degrees <= 247.5) {
 			direction = 5;
 			adjacent = _all.slice(5);
 		}
 		// G H
-		else if (degrees >= 247.5 && degrees <= 292.5) {
+		else if (degrees > 247.5 && degrees <= 292.5) {
 			direction = 6;
 			adjacent = [..._all.slice(6), _all[0]];
 		}
-		// H A
-		else if (degrees >= 292.5 && degrees <= 337.5) {
-			direction = 7;
-			adjacent = [_all[7], ..._all.slice(0,2)];
-		}
 
 		let mostNumberOfAgents = 0,
-			bestDirection = -1;
+			bestDirection = -5;
 
-		for (let dir = 0; dir < adjacent.length; dir++) {
+		for (let dir = -1; dir < adjacent.length; dir++) {
 			const numberOfAgentsInCell = quadrants[adjacent[dir]];
 			if (typeof numberOfAgentsInCell == "number" && numberOfAgentsInCell > mostNumberOfAgents) {
 				mostNumberOfAgents = numberOfAgentsInCell;
@@ -208,40 +174,43 @@ const CANVAS = new Canvas("#canvas-output", 900, 900, function(t){
 			}
 		}
 
+		bestDirection = bestDirection == -1 ? 7 : bestDirection;
 		bestDirection = bestDirection > 7 ? bestDirection - 8 : bestDirection;
 
-		if (bestDirection != -1 && direction != bestDirection) {// && direction != bestDirection) {
+		if (bestDirection != -5 && direction != bestDirection) {
 
 			let newAngle;
 
 			switch(bestDirection) {
 				case 0:
-					newAngle = new Vector2(agentSteerStrength, agentSteerStrength);
+					newAngle = new Vector2(0, agentSteerStrength);
 					break;
 				case 1:
-					newAngle  = new Vector2(0, agentSteerStrength);
-					break;
-				case 2:
-					newAngle  = new Vector2(-agentSteerStrength, agentSteerStrength);
-					break;
-				case 3:
 					newAngle  = new Vector2(-agentSteerStrength, 0);
 					break;
-				case 4:
-					newAngle  = new Vector2(-agentSteerStrength, -agentSteerStrength);
+				case 2:
+					newAngle  = new Vector2(-agentSteerStrength, 0);
 					break;
-				case 5:
+				case 3:
 					newAngle  = new Vector2(0, -agentSteerStrength);
 					break;
+				case 4:
+					newAngle  = new Vector2(0, -agentSteerStrength);
+					break;
+				case 5:
+					newAngle  = new Vector2(-agentSteerStrength, 0);
+					break;
 				case 6:
-					newAngle  = new Vector2(agentSteerStrength, -agentSteerStrength);
+					newAngle  = new Vector2(agentSteerStrength, 0);
 					break;
 				case 7:
-					newAngle  = new Vector2(agentSteerStrength, 0);
+					newAngle  = new Vector2(0, agentSteerStrength);
 					break;
 			}
 
-			const scalar = Vector2.from_number(deltaTime);//Vector2.multiply(Vector2.from_number(deltaTime), Vector2.random(0, 2));
+			const sign = bestDirection > direction ? -1 : 1;
+
+			const scalar = Vector2.multiply(Vector2.from_number(deltaTime * sign), Vector2.random(0, 1.5));
 
 			agents[i].angle = Vector2.add(agents[i].angle, Vector2.multiply(newAngle, scalar));
 
